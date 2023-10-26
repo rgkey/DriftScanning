ead458ca6fb8bf137e320b4b102b757f64efe5b3
Renee Key <45152240+rgkey@users.noreply.github.com>
2023-10-26 16:26:41 +1100

    Add files via upload

---
+++ Drift_Classify.py
@@ -0,0 +1,18 @@
+import astropy 
+import numpy as np
+import scipy as scp
+import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import math
+
+#for cosmic ray removal
+import astroscrappy
+
+from astropy.wcs import WCS
+
+'''PLACEHOLDER FUNCTION'''
+
+'''
+Use knowledge of the source shape on drifted image to find point-like sources of light'''
---
+++ Drift_Reastronm.py
@@ -0,0 +1,18 @@
+import astropy 
+import numpy as np
+import scipy as scp
+import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import math
+
+#for cosmic ray removal
+import astroscrappy
+
+from astropy.wcs import WCS
+
+'''PLACEHOLDER FUNCTION'''
+
+'''
+Use the hexapod repointings to make sure the WCS is correct from time-to-time'''
