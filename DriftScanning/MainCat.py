d50c178e7aa4c8e6a56984c5f71dafc8bbf31301
Renee Key <45152240+rgkey@users.noreply.github.com>
2024-01-20 15:17:57 +1100

    Update MainCat.py to import in utils ImageImport and turn catalogue generation into a function

--- DriftScanning/MainCat.py
+++ DriftScanning/MainCat.py
@@ -159,155 +159,43 @@ def select_reference_stars(data, WCS,pixscale, Num_target = 20, edge_crit = 0.05
         
     return(ref_sample)
 
-#read in instant calibration version of guided field (otherwise will need a debiasing step if using pointed raw images)
-#The instant calibration is done by NOIRLab community pipeline, should already have correction WCS solution.
-g_path = '/Users/reneekey/Documents/drift_testset/g_ooi_exp012923.fits'
-r_path = '/Users/reneekey/Documents/drift_testset/r_ooi_exp013253.fits'
+def catalogue_generator(ccd_name, filepath):
+  Image = ImageImport(ccd_name, filepath)
+  
+  CRTask = cosmicray_removal(Image)
+  REFtask = select_reference_stars(Image.data, Image.wcs, Image.hdr['PIXSCAL1'])
 
-#select two CCD names - Northern Edge (S31), Southern Middle (N16)
-ccd_names = ['N16', 'S31']
-i = 1
-
-with fits.open(g_path) as g_file:
-    g_hdr = g_file[0].header
-    g_img_hdr = g_file[ccd_names[i]].header
-    g_img_data = g_file[ccd_names[i]].data
-
-w_g = WCS(g_img_hdr)   #read in WCS of both images, this is corrected by NOIRLab
-
-with fits.open(r_path) as r_file:
-    r_hdr = r_file[0].header
-    r_img_hdr = r_file[ccd_names[i]].header
-    r_img_data = r_file[ccd_names[i]].data
-
-w_r = WCS(r_img_hdr)
-
-#Remove cosmic rays via LACosmic (use median combination of image values as better theshold estimate than the standard instrumental report from NOIRlab)
-gain_g = np.median([g_img_hdr['GAINA'], g_img_hdr['GAINB']], axis = 0)
-readnoise_g = np.median([g_img_hdr['RDNOISEA'], g_img_hdr['RDNOISEA']], axis = 0)
-satlevel_g = np.median([g_img_hdr['SATURATA'], g_img_hdr['SATURATB']], axis = 0)
-
-mask_g, data_g = astroscrappy.detect_cosmics(g_img_data, gain=gain_g, readnoise=readnoise_g, satlevel=satlevel_g, 
-                                            cleantype='medmask')
-
-gain_r = np.median([r_img_hdr['GAINA'], r_img_hdr['GAINB']], axis = 0)
-readnoise_r = np.median([r_img_hdr['RDNOISEA'], r_img_hdr['RDNOISEA']], axis = 0)
-satlevel_r = np.median([r_img_hdr['SATURATA'], r_img_hdr['SATURATB']], axis = 0)
-
-mask_r, data_r = astroscrappy.detect_cosmics(r_img_data, gain=gain_r, readnoise=readnoise_r, satlevel=satlevel_r, 
-                                            cleantype='medmask')
-
-
-
-'''SECTION A: find well behaved, bright, isolated stars:
-To use with building the PSF model for all stars'''
-
-g_stars_tbl = select_reference_stars(data_g, w_g, g_hdr['PIXSCAL1'])
-
-g_median_val = sigma_clipped_stats(data_g, sigma=2.0)[1]
-g_data_clean = data_g - g_median_val  
-
-g_nddata = NDData(data=g_data_clean) 
-g_stars = extract_stars(g_nddata, g_stars_tbl, size=25)
-
-'''TO DO: Build this into the select_reference_stars function'''
-#use only sources with circular morphology (removes saturated sources and galaxies)
-g_symmetry = []
-for star in g_stars:
-    cat = data_properties(star)
-    columns = ['label', 'xcentroid', 'ycentroid', 'semimajor_sigma',
-           'semiminor_sigma', 'orientation']
-    tbl = cat.to_table(columns=columns)
-    a = tbl['semimajor_sigma']
-    b = tbl['semiminor_sigma']
-    sym = a/b
-    g_symmetry.append(sym.value[0])
-g_iso_source = np.where(np.array(symmetry) < 1.1)[0]
-
-
-g_points = [g_stars[i].data for i in g_iso_source]
-g_fwhms = [np.median(FWHM_2dg(star)) for star in g_points]
-fwhm_init_g = sigma_clipped_stats(g_fwhms, sigma=3.0)[1]  #gives us the FWHM from the reference stars
-
-#Redo the above code for r-band image (Am in the process of removing this and make this whole section a function call in main)
-r_stars_tbl = select_reference_stars(data_r, w_r, r_hdr['PIXSCAL1'])
-
-r_median_val = sigma_clipped_stats(data_r, sigma=2.0)[1]
-r_data_clean = data_r - r_median_val  
-
-r_nddata = NDData(data=r_data_clean) 
-r_stars = extract_stars(r_nddata, r_stars_tbl, size=25)
-
-r_symmetry = []
-for star in r_stars:
-    cat = data_properties(star)
-    columns = ['label', 'xcentroid', 'ycentroid', 'semimajor_sigma',
-           'semiminor_sigma', 'orientation']
-    tbl = cat.to_table(columns=columns)
-    a = tbl['semimajor_sigma']
-    b = tbl['semiminor_sigma']
-    sym = a/b
-    r_symmetry.append(sym.value[0])
-r_iso_source = np.where(np.array(symmetry) < 1.1)[0]
-
-
-r_points = [r_stars[i].data for i in r_iso_source]
-r_fwhms = [np.median(FWHM_2dg(star)) for star in r_points]
-fwhm_init_r = sigma_clipped_stats(r_fwhms, sigma=3.0)[1]
-
-#Use FWHM to define the PSF sigma guess for the PRF model
-g_sigma_psf = fwhm_init_g * gaussian_fwhm_to_sigma
-
-#Run photometry on the CCD images
-fitter = LevMarLSQFitter()
-g_psf_model = IntegratedGaussianPRF(sigma=g_sigma_psf) 
-photometry_g = DAOPhotPSFPhotometry(crit_separation = 3*fwhm_init_g, threshold = thresh_init_g, 
-                                    fwhm = fwhm_init_g, sigma = 3, psf_model=g_psf_model, fitter=LevMarLSQFitter(),
+  #Use FWHM to define the PSF sigma guess for the PRF model
+  psf_fwhm = sigma_clipped_stats(REFtask['fwhm'].values, sigma=3.0)[1]
+  sigma_psf = psf_fwhm * gaussian_fwhm_to_sigma
+  background = Image.hdr['BACKGROUND'] #DEBUG THIS HEADER NAME
+  
+  #Run photometry on the CCD images
+  fitter = LevMarLSQFitter()
+  psf_model = IntegratedGaussianPRF(sigma=sigma_psf) 
+  photometry = DAOPhotPSFPhotometry(crit_separation = 3*psf_fwhm, threshold = background, 
+                                    fwhm = psf_fwhm, sigma = 3, psf_model=psf_model, fitter=LevMarLSQFitter(),
                                     niters=1, fitshape=(7,7))
 
-table_g = photometry_g(image=data_g)   
-residual_g = photometry_g.get_residual_image()
+  table = photometry(image=Image.data)   
+  residual = photometry.get_residual_image()
+  
+  #Use WCS convention to get estimate of local RA, Dec for X-match between r and g band
+  sky_positions = w.pixel_to_world(table_g['x_fit'], table_g['y_fit'])
+  
+  #Make final frame
+  photometry_dataframe = pd.DataFrame()
+  comb_phot['chp'] = [ccd_names[i]]*len(w)           
+  comb_phot['id'] = table['id'][match]
+  comb_phot['x'] = table['x_fit'][match]
+  comb_phot['y'] = table['y_fit'][match]
+  comb_phot['flux'] = table['flux_fit'][match]
+  comb_phot['flux_unc'] = table['flux_unc'][match]
+  comb_phot['ra'] = np.array([i.ra.degree for i in w])[match]
+  comb_phot['dec'] = np.array([i.dec.degree for i in w])[match]
+  
+  return(comb_phot)
 
-#background threshold changes for r_band as exposure is longer
-r_sigma_psf = fwhm_init_r * gaussian_fwhm_to_sigma
-r_psf_model = IntegratedGaussianPRF(sigma=r_sigma_psf)
-photometry_r = DAOPhotPSFPhotometry(crit_separation = 3*fwhm_init_r, threshold = thresh_init_r, 
-                                    fwhm = fwhm_init_r, sigma = 3, psf_model=r_psf_model, fitter=LevMarLSQFitter(),
-                                    niters=1, fitshape=(7,7))
 
-table_r = photometry_r(image=data_r)   
-residual_r = photometry_r.get_residual_image()
-
-#Use WCS convention to get estimate of local RA, Dec for X-match between r and g band
-wcs_g = w_g.pixel_to_world(table_g['x_fit'], table_g['y_fit'])
-wcs_r = w_r.pixel_to_world(table_r['x_fit'], table_r['y_fit'])
-match, sep, d3sep = match_coordinates_sky(wcs_r, wcs_g) 
-
-#Make final frame
-comb_phot = pd.DataFrame()
-
-#Global info
-comb_phot['chp'] = [ccd_names[i]]*len(wcs_r)
-comb_phot['skysep_rg'] = sep.deg             
-
-#g-band info
-comb_phot['id_g'] = table_g['id'][match]
-comb_phot['x_g'] = table_g['x_fit'][match]
-comb_phot['y_g'] = table_g['y_fit'][match]
-comb_phot['flux_g'] = table_g['flux_fit'][match]
-comb_phot['flux_unc_g'] = table_g['flux_unc'][match]
-comb_phot['ra_g'] = np.array([i.ra.degree for i in wcs_g])[match]
-comb_phot['dec_g'] = np.array([i.dec.degree for i in wcs_g])[match]
-
-#r-band info
-comb_phot['id_r'] = table_r['id']
-comb_phot['x_r'] = table_r['x_fit']
-comb_phot['y_r'] = table_r['y_fit']
-comb_phot['flux_r'] = table_r['flux_fit']
-comb_phot['flux_unc_r'] = table_r['flux_unc']
-comb_phot['ra_r'] = np.array([i.ra.degree for i in wcs_r])
-comb_phot['dec_r'] = np.array([i.dec.degree for i in wcs_r])
-
-'''This is where a GAIA TAP query should X_match between local and external frames'''
-#send comb_phot to csv file
+#Call this in the main function
 comb_phot.to_csv('catalogue_photometry_S31.csv', sep = ',')
